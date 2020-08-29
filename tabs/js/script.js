5659424685f4907195e5ab41fd6cad93b5582c4b
MrBogdan007 <bogdanbrohovetsky@gmail.com>
2020-08-29 20:08:07 +0300

    sda

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -91,4 +91,56 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
     setClock('.timer', deadline);
-});
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+            modal = document.querySelector(".modal"),
+            modalCloseBtn = document.querySelector('[data-close]');
+       
+        function showModal() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+            clearInterval(modalTimerId);
+        }
+        function hideModal () {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            
+        } 
+        modalTrigger.forEach(i => {
+            i.addEventListener('click', () => {
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+            });
+        });
+       
+        modalCloseBtn.addEventListener('click', hideModal);
+            
+ 
+
+        modal.addEventListener('click', (e) =>{
+            if(e.target === modal){
+           hideModal();
+            }
+        });
+
+        document.addEventListener("keydown",(e) => {
+            if(e.code === 'Escape' && modal.classList.contains('show')){
+                hideModal();
+            }
+        } );
+
+        const modalTimerId = setTimeout(showModal , 3000);
+
+        function showModalEnd() {
+            if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+                showModal();
+                window.removeEventListener('scroll', showModalEnd);
+            }
+        }
+        window.addEventListener('scroll', showModalEnd);
+}); 
